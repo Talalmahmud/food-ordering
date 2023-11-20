449422f6e0046198d53e95c8b5297fce6c54d651
Ghuddy <mahmudtalal2@gmail.com>
2023-11-20 06:29:32 +0600

    gitignor env fix

--- next.config.js
+++ next.config.js
@@ -13,15 +13,6 @@ const nextConfig = {
       },
     ],
   },
-  env: {
-    MONGO_URL:
-      "mongodb+srv://mahmudtalal2:Osthir.2024@cluster0.mhqlp9e.mongodb.net/?retryWrites=true&w=majority",
-    NEXTAUTH_URL: "http://localhost:3000/",
-    SECRET: "talalmahmud",
-    GOOGLE_CLIENT_ID:
-      "344803160981-jsltvfrbs7ommpb01ua7mhblgn7q3fvl.apps.googleusercontent.com",
-    GOOGLE_CLIENT_SECRET: "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",
-  },
 };
 
 module.exports = nextConfig;
