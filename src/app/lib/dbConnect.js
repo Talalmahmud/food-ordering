bd5003664dd033cb7af9f4faa1afa1a961a9a84e
Ghuddy <mahmudtalal2@gmail.com>
2023-11-24 23:41:46 +0600

    changes

--- src/app/lib/dbConnect.js
+++ src/app/lib/dbConnect.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("Database is connected......");
   } catch (error) {
     console.log(error);
